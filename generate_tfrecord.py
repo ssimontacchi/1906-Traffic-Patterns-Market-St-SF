0abfbc0b3e318b798823e3485d69da96e2e2498d
ssimontacchi <scott.j.simontacchi@gmail.com>
2018-11-13 14:17:18 -0800

    used generate_tfrecord to get TFRecords

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,10 +1,11 @@
 """
 Usage:
-  # From tensorflow/models/
+  # Adapted from tensorflow/models/
   # Create train data:
   python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=train.record
   # Create test data:
   python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=test.record
+  # Adapted from datitran's raccoon dataset
 """
 from __future__ import division
 from __future__ import print_function
@@ -16,7 +17,7 @@ import pandas as pd
 import tensorflow as tf
 
 from PIL import Image
-from dataset_util import *
+import dataset_util
 from collections import namedtuple, OrderedDict
 
 flags = tf.app.flags
@@ -92,7 +93,7 @@ def create_tf_example(group, path):
 
 def main(_):
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-    path = os.path.join(os.getcwd(), 'images')
+    path = os.path.join(os.getcwd(), 'training_data/JPEGImages')
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
     for group in grouped:
