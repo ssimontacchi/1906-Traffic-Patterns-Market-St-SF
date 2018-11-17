ca3ea31a72b55c6903043df1ac9cbf5629a9686f
ssimontacchi <scott.j.simontacchi@gmail.com>
2018-11-16 22:52:42 -0800

    lucas kanade for optical flow

--- run_cloud.sh
+++ run_cloud.sh
@@ -21,11 +21,26 @@ gcloud ml-engine jobs submit training object_detection_eval_`date +%s` \
 
 # better!:
 gcloud ml-engine jobs submit training object_detection_`date +%s` \
+    --runtime-version 1.2 \
     --job-dir=gs://market_st_1906/train \
     --packages dist/object_detection-0.1.tar.gz,slim/dist/slim-0.1.tar.gz,cocoapi/pycocotools-2.0.tar.gz \
-    --module-name object_detection.train \
+    --module-name object_detection.model_main \
     --region us-east1 \
     --config /Users/Scott/models/research/object_detection/samples/cloud/cloud.yml \
     -- \
     --train_dir=gs://market_st_1906/train \
     --pipeline_config_path=gs://market_st_1906/data/faster_rcnn_inception_v2_coco.config
+
+# from support
+gcloud ml-engine jobs submit training `whoami`_object_detection_`date +%m_%d_%Y_%H_%M_%S` \
+--job-dir=gs://market_st_1906/train \
+--packages dist/object_detection-0.1.tar.gz,slim/dist/slim-0.1.tar.gz,/tmp/pycocotools/pycocotools-2.0.tar.gz \
+--module-name object_detection.model_tpu_main \
+--runtime-version 1.9 \
+--scale-tier BASIC_TPU \
+--region us-central1 \
+--config /Users/Scott/models/research/object_detection/samples/cloud/cloud.yml \
+-- \
+--tpu_zone us-central1 \
+--model_dir=gs://market_st_1906/train \
+--pipeline_config_path=gs://market_st_1906/data/faster_rcnn_inception_v2_coco.config
